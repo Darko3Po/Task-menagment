e5c1c504267a9c0c4697e479edecb4d23a83e9c3
Darko3Po <potajacdarko9@gmail.com>
2023-12-13 17:13:06 +0100

    Laravel auth added

---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container d-flex justify-content-between w-100">
+      <div class="col-md-8">
+          <a class="navbar-brand" href="#">Navbar scroll</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+      </div>
+        <div class="collapse navbar-collapse col-md-4" id="navbarScroll">
+             <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
